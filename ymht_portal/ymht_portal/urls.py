712e50ac6b11a6b5f66c8e1406f0f1cfff147bfe
Smita Korgaonkar <smitak099@gmail.com>
2014-06-24 12:14:26 +0530

    #5 solve media upload error, #10 cordinator display issue, done auto select username for Ymht model

--- ymht_portal/ymht_portal/urls.py
+++ ymht_portal/ymht_portal/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-)
+    
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
