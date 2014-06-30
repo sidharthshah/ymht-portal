e7ab41438c8567ccbc75703db06f0250c33beb80
Smita Korgaonkar <smitak099@gmail.com>
2014-06-30 17:11:35 +0530

    #15 #7 add gender , education details job details, hobby and event in ymht

--- ymht_portal/ymht/admin.py
+++ ymht_portal/ymht/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import (YMHT, Coordinator, YMHTMobile,
-                    YMHTEmail, YMHTAddress, City,
+from .models import (YMHT, Coordinator, YMHTMobile, Hobby,
+                    YMHTEmail, YMHTAddress, YMHTEducation, YMHTJob, City,
                     State, Country, Center, Membership,
                     Event, SevaDetails)
 
@@ -15,35 +15,45 @@ class YMHTEmailInline(admin.TabularInline):
 class YMHTAddressInline(admin.TabularInline):
     model = YMHTAddress
 
+class YMHTEducationInline(admin.TabularInline):
+    model = YMHTEducation
+
+class YMHTJobInline(admin.TabularInline):
+    model = YMHTJob
+
 class YMHTMembershipInline(admin.TabularInline):
     fields = ('ymht' , 'center' , 'coordinator', 'age_group' , 'role', 'since', 'till')
     model = Membership
 
 class YMHTSevaDetailsInline(admin.TabularInline):
-    fields = ('event', 'ymht' , 'attended' , 'attended_days' , 'comments')
+    fields = ('event', 'ymht' , 'coordinator', 'attended' , 'attended_days' , 'comments')
     model = SevaDetails
 
+
 class YMHTAdmin(admin.ModelAdmin):
-    exclude = ('user',)
     list_display = ('first_name', 'last_name', 'date_of_birth', 'gnan_date')
     inlines = [
         YMHTMobileInline,
         YMHTEmailInline,
         YMHTAddressInline,
+        YMHTEducationInline,
+        YMHTJobInline,
         YMHTMembershipInline,
         YMHTSevaDetailsInline
     ]
 
     def save_model(self, request, obj, form, change):
         if not change:
-            obj.user = request.user 
+            obj.user = request.user
             obj.save()
+
             
 admin.site.register(Country)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Event)
 admin.site.register(Center)
+admin.site.register(Hobby)
 admin.site.register(YMHT, YMHTAdmin)
 admin.site.register(Coordinator)
 admin.site.register(Membership)
--- ymht_portal/ymht/models.py
+++ ymht_portal/ymht/models.py
@@ -4,33 +4,11 @@ from django.contrib.auth.models import User
 def session_content_file_name(instance, filename):
     return '/'.join(['session', instance.user.username, filename])
 
-class YMHT(models.Model):
-  user = models.ForeignKey(User, blank=True, null=True)
-  first_name = models.CharField(max_length=255)
-  last_name = models.CharField(max_length=255)
-  date_of_birth = models.DateField()
-  gnan_date = models.DateField(blank=True, null=True)
-  father_name = models.CharField(max_length=255)
-  father_contact = models.CharField(max_length=10, blank=True, null=True)
-  mother_name = models.CharField(max_length=255)
-  mother_contact = models.CharField(max_length=10, blank=True, null=True)
-  profile_picture = models.ImageField(upload_to=session_content_file_name, blank=True, null=True)
+class Hobby(models.Model):
+  title = models.CharField(max_length=255)
 
   def __unicode__(self):
-    return '%s %s' % (self.first_name, self.last_name)
-
-  class Meta:
-    verbose_name_plural = "YMHTians"
-
-class YMHTMobile(models.Model):
-  ymht = models.ForeignKey(YMHT)
-  mobile = models.CharField(max_length=255)
-  is_active = models.BooleanField(default=False)
-
-class YMHTEmail(models.Model):
-  ymht = models.ForeignKey(YMHT)
-  email = models.EmailField()
-  is_active = models.BooleanField(default=False)
+    return '%s' % (self.title)
 
 class Country(models.Model):
   name = models.CharField(max_length=255)
@@ -59,6 +37,60 @@ class City(models.Model):
   class Meta:
     verbose_name_plural = 'Cities'
 
+EVENT_CATEGORY_CHOICES = ((0, 'GNC Day'),
+                    (1, 'Summer Camp'),
+                    (2, 'YUVA Camp'),
+                    (3, 'Aptaputra Satsang'),
+                    (4, 'General Satsang'),
+                    (5, 'Parayan'),
+                    (6, 'Janm Jayanti'),
+                    (7, 'Picnic'))
+
+class Event(models.Model):
+  held_on = models.DateField()
+  category = models.IntegerField(choices=EVENT_CATEGORY_CHOICES)
+  location = models.ForeignKey(City)
+
+  def __unicode__(self):
+    return '%s: %s, %s' % (dict(EVENT_CATEGORY_CHOICES).get(self.category), self.held_on, self.location)
+
+
+class YMHT(models.Model):
+  GENDER_CHOICES = (
+      ('male', "Male"),
+      ('female', "Female"),)
+  user = models.ForeignKey(User, blank=True, null=True)
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
+  gender = models.CharField(max_length=25, blank=False, null=False, choices=GENDER_CHOICES, default='male')
+  date_of_birth = models.DateField()
+  hobby = models.ForeignKey(Hobby)
+  event = models.ForeignKey(Event)
+  gnan_date = models.DateField(blank=True, null=True)
+  father_name = models.CharField(max_length=255)
+  father_contact = models.CharField(max_length=10, blank=True, null=True)
+  mother_name = models.CharField(max_length=255)
+  mother_contact = models.CharField(max_length=10, blank=True, null=True)
+  profile_picture = models.ImageField(upload_to=session_content_file_name, blank=True, null=True)
+
+  def __unicode__(self):
+    return '%s %s' % (self.first_name, self.last_name)
+
+  class Meta:
+    verbose_name_plural = "YMHTians"
+
+class YMHTMobile(models.Model):
+  ymht = models.ForeignKey(YMHT)
+  mobile = models.CharField(max_length=255)
+  is_active = models.BooleanField(default=False)
+
+class YMHTEmail(models.Model):
+  ymht = models.ForeignKey(YMHT)
+  email = models.EmailField()
+  is_active = models.BooleanField(default=False)
+
+
+
 class YMHTAddress(models.Model):
   ymht = models.ForeignKey(YMHT)
   address_1 = models.CharField(max_length=255)
@@ -68,6 +100,29 @@ class YMHTAddress(models.Model):
   city = models.ForeignKey(City)
   zipcode = models.CharField(max_length=6)
   is_active = models.BooleanField(default=False)
+
+class YMHTEducation(models.Model):
+  ymht = models.ForeignKey(YMHT)
+  college = models.CharField(max_length=255)
+  degree = models.CharField(max_length=255)
+  other_details = models.TextField()
+  year = models.CharField(max_length=255)
+
+  def __unicode__(self):
+    return '%s' % (self.degree)
+
+class YMHTJob(models.Model):
+  ymht = models.ForeignKey(YMHT)
+  jobtype = models.CharField(max_length=255)
+  experience = models.IntegerField()
+  company_name = models.CharField(max_length=255)
+  designation = models.CharField(max_length=255)
+  job_category = models.CharField(max_length=255)
+  current = models.BooleanField()
+
+  def __unicode__(self):
+    return '%s' % (self.company_name)
+    
 
 class Coordinator(models.Model):
   first_name = models.CharField(max_length=255)
@@ -115,24 +170,6 @@ class Membership(models.Model):
   since = models.DateField()
   till = models.DateField()
 
-EVENT_CATEGORY_CHOICES = ((0, 'GNC Day'),
-                    (1, 'Summer Camp'),
-                    (2, 'YUVA Camp'),
-                    (3, 'Aptaputra Satsang'),
-                    (4, 'General Satsang'),
-                    (5, 'Parayan'),
-                    (6, 'Janm Jayanti'),
-                    (7, 'Picnic'))
-
-class Event(models.Model):
-
-
-  held_on = models.DateField()
-  category = models.IntegerField(choices=EVENT_CATEGORY_CHOICES)
-  location = models.ForeignKey(City)
-
-  def __unicode__(self):
-    return '%s: %s, %s' % (dict(EVENT_CATEGORY_CHOICES).get(self.category), self.held_on, self.location)
 
 class SevaDetails(models.Model):
   ATTENDED_DETAILS = ((1, 'All Days'), (2, 'Partial Days'))
